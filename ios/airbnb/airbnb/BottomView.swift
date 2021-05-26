c019e3a351b442dc0d7f43b65acec568bc84b9ed
zeke <yangsin1031@naver.com>
2021-05-26 22:07:18 +0900

    feat: xib을 이용하여 BottomView생성

---
+++ ios/airbnb/airbnb/BottomView.swift
@@ -0,0 +1,39 @@
+//
+//  BottomView.swift
+//  airbnb
+//
+//  Created by 양준혁 on 2021/05/26.
+//
+
+import UIKit
+
+class BottomView: UIView {
+
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetUp()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        xibSetUp()
+        rightButton.isEnabled = false
+    }
+    
+    private func xibSetUp() {
+        guard let view = loadViewFromNib(nib: "BottomView") else { return }
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(view)
+    }
+    
+    private func loadViewFromNib(nib: String) -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nib, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+
+}
