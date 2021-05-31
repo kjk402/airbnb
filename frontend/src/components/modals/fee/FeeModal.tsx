c62cbe541792649cdb1061862889803f4101f139
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-05-31 17:18:44 +0900

    style: 요금 모달 정적 UI

--- frontend/src/components/modals/fee/FeeModal.tsx
+++ frontend/src/components/modals/fee/FeeModal.tsx
@@ -20,11 +20,43 @@ export default function FeeModal({ type, setInplaceHolder, isActive, setModalOn 
 		<>
 			{isActive && (
 				<ModalContainer type={type} onClick={(e) => handleOnclick(e)}>
-					<ContentWrapper>요금모달~</ContentWrapper>
+					<ContentWrapper>
+						<Title>가격 범위</Title>
+
+						<PriceRange>100,000 - 1,000,000+</PriceRange>
+						<PriceAvg>평균 1박 요금은 165,556 입니다.</PriceAvg>
+
+						<StyleGraph>그래프</StyleGraph>
+					</ContentWrapper>
 				</ModalContainer>
 			)}
 		</>
 	);
 }
 
-const ContentWrapper = styled.div``;
+const ContentWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 75%;
+	height: 80%;
+`;
+
+const Title = styled.div`
+	padding: 20px 0;
+`;
+
+const PriceRange = styled.div`
+	font-size: 18px;
+`;
+
+const PriceAvg = styled.div`
+	color: #828282;
+	line-height: 40px;
+`;
+
+const StyleGraph = styled.div`
+	width: 365px;
+	height: 100px;
+	border: 1px solid red;
+	margin: 30px 0;
+`;
