d3295db4161b024eb7220adca12b31a33f786c52
zeke <yangsin1031@naver.com>
2021-06-01 14:50:57 +0900

    feat: RangeSlider구현

---
+++ ios/airbnb/airbnb/Controller/RangeSliderTrackLayer.swift
@@ -0,0 +1,12 @@
+//
+//  RangeSliderTrackLayer.swift
+//  airbnb
+//
+//  Created by 양준혁 on 2021/05/31.
+//
+
+import UIKit
+
+class RangeSliderTrackLayer: CALayer {
+    weak var rangeSlider: RangeSlider?
+}
