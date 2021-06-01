ea2925271a5d4acf7178cd818f5c92dbab494a29
zeke <yangsin1031@naver.com>
2021-06-01 16:31:29 +0900

    feat: PersonnelView 생성 및 오토레이아웃 설정

---
+++ ios/airbnb/airbnb/PersonnelView.swift
@@ -0,0 +1,20 @@
+//
+//  PersonnelView.swift
+//  airbnb
+//
+//  Created by 양준혁 on 2021/06/01.
+//
+
+import UIKit
+
+class PersonnelView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
