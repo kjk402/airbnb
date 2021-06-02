ec813e02ea773848ef2a819d058a258944125d9f
zeke <yangsin1031@naver.com>
2021-06-02 15:29:28 +0900

    feat: PersonnelView xib으로 구현

--- ios/airbnb/airbnb/PersonnelView.swift
+++ ios/airbnb/airbnb/PersonnelView.swift
@@ -8,13 +8,38 @@
 import UIKit
 
 class PersonnelView: UIView {
+    
+    @IBOutlet weak var theNumberOfAdult: UILabel!
+    @IBOutlet weak var theNumberOfKid: UILabel!
+    @IBOutlet weak var theNumberOfInfant: UILabel!
+    @IBOutlet weak var addAdultButton: UIButton!
+    @IBOutlet weak var addKidButton: UIButton!
+    @IBOutlet weak var addInfantButton: UIButton!
+    @IBOutlet weak var subtractAdultButton: UIButton!
+    @IBOutlet weak var subtractKidButton: UIButton!
+    @IBOutlet weak var subtractInfantButton: UIButton!
+    
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setXib()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setXib()
+    }
+    
+    func setXib() {
+        guard let view = loadViewFromNib(nib: "PersonnelView") else { return }
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(view)
+    }
+    
+    func loadViewFromNib(nib: String) -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nib, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
 }
