885635fc0428eef15b53c17955434b13b9281867
zeke <yangsin1031@naver.com>
2021-06-04 15:10:03 +0900

    feat: OAuth화면의 GradientAnimation을 가진 배경 추가

--- ios/airbnb/airbnb/OAuthViewController.swift
+++ ios/airbnb/airbnb/OAuthViewController.swift
@@ -19,6 +19,7 @@ class OAuthViewController: UIViewController, ASWebAuthenticationPresentationCont
         let networkManager = NetworkManager()
         oauthManager = OAuthManager(networkManager: networkManager)
         initializeWebAuthSession()
+        setBackgroundAnimation()
     }
     
     @IBAction func loginButtonTapped(_ sender: Any) {
@@ -34,5 +35,25 @@ class OAuthViewController: UIViewController, ASWebAuthenticationPresentationCont
         self.webAuthSession = oauthManager?.authenticate(completion: {
                 self.performSegue(withIdentifier: "TabBarController", sender: self)
         })
+    }
+    
+    private func setBackgroundAnimation() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        
+        let colors: [CGColor] = [ #colorLiteral(red: 0.4989262223, green: 0.2519669533, blue: 0.7005652785, alpha: 1), #colorLiteral(red: 0.8642135859, green: 0.2082472444, blue: 0.4913758636, alpha: 1), #colorLiteral(red: 0.99425596, green: 0.781129241, blue: 0.398478359, alpha: 1) ]
+        gradientLayer.colors = colors
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        
+        let changedColors: [CGColor] = [ #colorLiteral(red: 0.99425596, green: 0.781129241, blue: 0.398478359, alpha: 1), #colorLiteral(red: 0.8642135859, green: 0.2082472444, blue: 0.4913758636, alpha: 1), #colorLiteral(red: 0.4989262223, green: 0.2519669533, blue: 0.7005652785, alpha: 1) ]
+        let colorAnimation = CABasicAnimation(keyPath: "colors")
+        colorAnimation.toValue = changedColors
+        colorAnimation.duration = 3
+        colorAnimation.autoreverses = true
+        colorAnimation.repeatCount = .infinity
+        
+        gradientLayer.add(colorAnimation, forKey: "colorChangeAnimation")
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
